15cc22db6862fa87db453be6290fab145189a397
AlphaMiao(ฅ'ω'ฅ)~ <72894971+AlphaMiao-FJH@users.noreply.github.com>
2022-04-02 11:08:12 +0800

    Update svm.h

--- include/svm.h
+++ include/svm.h
@@ -1,30 +1,46 @@
-#include <time.h>  
-#include <stdio.h>  
-#include <iostream> 
-#include <opencv2/ml/ml.hpp>  
-#include <opencv2/opencv.hpp>  
-#include <opencv2/core/core.hpp>  
-#include <opencv2/highgui/highgui.hpp>  
+#include "../AngelSolver/AngleSolver.hpp"
+#include "../General/General.h"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 using namespace ml;
 
-class GetNum
+const string model_path = "../File/HOG_SVM.xml";
+
+/* hog-svm定义 */
+class HOG_SVM
+{
+private:
+    cv::Ptr<cv::ml::SVM> m_svm;
+    std::map<int, int> m_label2id;
+    cv::HOGDescriptor m_hog;
+
+public:
+    HOG_SVM();
+    int test(const cv::Mat &src);
+};
+
+class NumClassfier
 {
 public:
-    void initSvm(String file_name)
-    {
-        this->svm_model = SVM::create();
-        this->svm_model = Algorithm::load<SVM>(file_name);
-        if(svm_model.empty())
-        {
-            cout<< "file is empty! " << endl;
-        }
-    }
-
-    int getnum(Mat src, RotatedRect roi);
-
+    bool runSvm(const Mat &src,ArmorPlate &target_armor);
+    NumClassfier();
+    HOG_SVM m_svm;                  //数字识别类
+    int num;                        //数字
 private:
-    cv::Ptr<cv::ml::SVM> svm_model;
+    Ptr<SVM> svm;
+
+
+    Point2f src_apex_cord[4];
+    Point2f dst_apex_cord[4];
+
+    int sample_size;                                     //设置样本大小
+    int binary_threshold;                                //设置二值化阈值
+    
+    bool loadSvmModel(const string &path);               //SVM模型加载
+    bool initImg(const Mat &src,Mat &dst,const ArmorPlate &target_armor);
 };
+
